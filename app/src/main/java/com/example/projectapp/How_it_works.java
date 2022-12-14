4e1146d35101d3fb3d5a6a71e44027686d98b53a
DariInel <adara21210@gmail.com>
2022-12-14 23:55:05 +0500

    Сделана стр со статьей + исправлены ошибки

---
+++ app/src/main/java/com/example/projectapp/How_it_works.java
@@ -0,0 +1,14 @@
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class How_it_works extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_it_works);
+    }
+}
