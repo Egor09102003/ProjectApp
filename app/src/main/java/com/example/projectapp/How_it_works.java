82a3dbd156c554675e407125e978cdf5acf14657
DariInel <adara21210@gmail.com>
2023-01-15 16:50:47 +0500

    Доделана стр "Как это работает"

--- app/src/main/java/com/example/projectapp/How_it_works.java
+++ app/src/main/java/com/example/projectapp/How_it_works.java
@@ -2,7 +2,9 @@ package com.example.projectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class How_it_works extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class How_it_works extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_it_works);
+    }
+    public void startMain(View view) {
+        Intent intent = new Intent(this, Main_page.class);
+        startActivity(intent);
     }
 }
