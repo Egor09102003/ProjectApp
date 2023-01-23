9c451abc74ea7ff5f3627307059be18821b9b060
DariInel <adara21210@gmail.com>
2023-01-24 03:22:30 +0500

    Сделана стр с почтой

--- app/src/main/java/com/example/projectapp/Mailing.java
+++ app/src/main/java/com/example/projectapp/Mailing.java
@@ -2,7 +2,13 @@ package com.example.projectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Mailing extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class Mailing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mailing);
+    }
+
+    public void startMain(View view) {
+        String answer = ((TextInputEditText) findViewById(R.id.input1)).getText().toString();
+        if ((!answer.isEmpty() && isEmailValid(answer)) || answer.isEmpty()) {
+            Intent intent = new Intent(this, Results.class);
+            startActivity(intent);
+        } else {
+            TextView textWarning = (TextView) findViewById(R.id.textViewError);
+            textWarning.setText("Пожалуйста, введите корректный адрес");
+        }
+
+    }
+
+    public boolean isEmailValid(String email){
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
